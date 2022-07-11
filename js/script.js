e7b804c7237b4a014a8c2722f4192ade0b03d9ee
Geziael Thiago <80595185+GeziaelThiagoPaes@users.noreply.github.com>
2022-07-10 23:11:08 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,69 +1,89 @@
+let battle = document.querySelector('#battle');
+let gamearray = ["rock", "paper", "scissors"];
+let btn_restart = document.querySelector('#btn_restart')
+const all_Game_Buttons = document.querySelectorAll('button');
+let playerScore = 0;
+let computerScore = 0;
+let count = 0;
 
-let gamearray = ["ROCK", "PAPER", "SCISSORS"];
+all_Game_Buttons.forEach(all_Game_Buttons => all_Game_Buttons.addEventListener('click', (e) => {
+
+    let scoreboard = document.querySelector('#scoreboard');
+    let winner = document.querySelector('#winner');
+    let machine = document.querySelector('#machine');
+    let round = document.querySelector('#round');
+    let userchoice;
+
+
+    userchoice = e.target.id;
+    round.textContent = 'Round : ' + (count + 1);
+
+    let round_value;
+    let machine_Choice = computerPlay();
+
+    if (playerScore < 5 && computerScore < 5) {
+        round_value = playRound(userchoice, machine_Choice);
+        scoreboard.textContent = " User : " + playerScore + " Vs " + " Machine : " + computerScore;
+       
+
+
+
+    } if (playerScore == 5) {
+        winner.textContent = 'Congratulations, you win!';
+        scoreboard.textContent = " User : " + playerScore + " X " + " Machine : " + computerScore;
+        alert('Congratulations, you win!', location.reload());
+
+
+
+    } else if (computerScore == 5) {
+        winner.textContent = 'Machine Win!';    
+        scoreboard.textContent = " User : " + playerScore + " X " + " Machine : " + computerScore;
+        alert('I\`m so sorry, you lose!', location.reload());
+    }
+
+    machine.textContent = round_value;
+
+
+}));
+btn_restart.addEventListener('click', () => {
+    location.reload();
+})
+
+
+function playRound(userchoice, computerSelection) {
+
+    if (userchoice == computerSelection) {
+        playerScore = playerScore;
+        computerScore = computerScore;
+        count++;
+        return (`Tie!`);
+
+    }
+    else if ((userchoice == "rock" && computerSelection == gamearray[1]) ||
+        (userchoice == "scissors" && computerSelection == gamearray[0]) ||
+        (userchoice == "paper" && computerSelection == gamearray[2])) {
+        computerScore++;
+        playerScore = playerScore;
+        count++;
+        return (`You Lose!`);
+
+    }
+    if ((userchoice == "rock" && computerSelection == gamearray[2]) ||
+        (userchoice == "scissors" && computerSelection == gamearray[1]) ||
+        (userchoice == "paper" && computerSelection == gamearray[0])) {
+        playerScore++;
+        computerScore = computerScore;
+        count++;
+        return `You Won!`;
+
+    };
+
+}
+
 function computerPlay() {
     var t = Math.floor(Math.random() * gamearray.length)
     let rvalue = gamearray[t];
     return rvalue;
 }
-
-const computerSelection = computerPlay();
-let playerSelection;
-let playerScore = 0;
-let computerScore = 0;
-
-function game() {
-    for (let i = 0; i < 5; i++) {
-        playerSelection = prompt("Rock, Paper or Scissors?")
-        const computerRamdon = computerPlay();
-        console.log(playRound(playerSelection.toUpperCase(), computerRamdon))
-
-    }
-    function winScore() {
-        if (playerScore > computerScore) {
-            console.log(`Player won  `)
-
-        }
-        else if (computerScore > playerScore) {
-            console.log(`computer won `)
-
-
-        }
-        else {
-            console.log(`Tie`)
-
-        }
-    }
-    winScore();
-}
-
-
-
-
-function playRound(playerSelection, computerSelection) {
-
-    if (playerSelection == computerSelection) {
-        playerScore = playerScore;
-        computerScore = computerScore;
-        return (`Tie : ${playerSelection} X ${computerSelection}`);
-    }
-    else if ((playerSelection === "ROCK" && computerSelection == gamearray[1]) ||
-        (playerSelection == "SCISSORS" && computerSelection == gamearray[0]) ||
-        (playerSelection == "PAPER" && computerSelection == gamearray[2])) {
-        computerScore++;
-        playerScore = playerScore - 1;
-        return (`You Lose! : ${playerSelection} X ${computerSelection}`);
-
-    }
-    else {
-        playerScore++;
-        computerScore = computerScore - 1;
-        return `The player won : ${playerSelection} X ${computerSelection}`;
-
-    }
-
-
-}
-
-game();
 
 
